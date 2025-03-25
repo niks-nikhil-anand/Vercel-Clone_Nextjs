8de7d09ab538082d0dfb86bb14396361d17df2dd
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-03-26 00:01:42 +0530

    Added S3 CLient

--- src/script/script.mjs
+++ src/script/script.mjs
@@ -1,6 +1,16 @@
 import { exec } from "child_process";
 import path from "path";
 import fs from "fs";
+import {S3Client , PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+
+
+const S3Client = new S3Client({
+    region: "ap-south-1",
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+})
 
 
 
@@ -23,7 +33,11 @@ async function init() {
         console.log(`Build Complete ${data}`);
         const distFolderPath = path.join(outDirPath, "output" , "dist");
         const files = fs.readdirSync(distFolderPath , {recursive: true});
-        
+
+        for(const filePath of files){
+            if(fs.lstatSync(filePath).isDirectory()) continue;
+
+        }
 
     });
 
